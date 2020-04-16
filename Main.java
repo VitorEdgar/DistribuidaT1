0e0d624bcb709142897ad5d361c9a89e114332c3
gabriel.moraes <gabriel.moraes@dbserver.com.br>
2020-04-16 20:01:53 -0300

    Cliente guardando host

--- Main.java
+++ Main.java
@@ -21,48 +21,65 @@ public class Main {
             System.out.println("Cliente");
             System.out.println(grupo.getHostAddress());
 
+            InetAddress host;
+
             byte[] saida = new byte[1024];
             saida = ("[" + nick + "] " + " /all/ Ola").getBytes();
             pacote = new DatagramPacket(saida, saida.length, grupo, 5000);
             socket.send(pacote);
-            while(true){
+            while (true) {
                 try {
                     byte[] entrada = new byte[1024];
-                    pacote = new DatagramPacket(entrada,entrada.length);
+                    pacote = new DatagramPacket(entrada, entrada.length);
                     socket.setSoTimeout(500);
                     socket.receive(pacote);
-                    String recebido = new String(pacote.getData(),0,pacote.getLength());
-                    
+                    String recebido = new String(pacote.getData(), 0, pacote.getLength());
+
                     String vars[] = recebido.split("/");
                     try {
                         System.out.println("Name: " + vars[1]);
                         if (vars[1].equals(nick)) {
                             System.out.println("Received: " + recebido);
+                            if(recebido.contains("Host")){
+                                host = pacote.getAddress();
+
+                                System.out.println(host.toString());
+                            }
                             if ("fim".equals(recebido))
                                 break;
                         }
                     } catch (ArrayIndexOutOfBoundsException e) {
                     }
-                } catch (IOException e){
+                } catch (IOException e) {
                 }
             }
         } else {
             System.out.println("Servidor");
             System.out.println(grupo.getHostAddress());
 
-            while(true){
+            while (true) {
                 try {
                     byte[] entrada = new byte[1024];
-                    pacote = new DatagramPacket(entrada,entrada.length);
+                    pacote = new DatagramPacket(entrada, entrada.length);
                     socket.setSoTimeout(500);
                     socket.receive(pacote);
-                    String recebido = new String(pacote.getData(),0,pacote.getLength());
-                    
+                    String recebido = new String(pacote.getData(), 0, pacote.getLength());
+
                     String vars[] = recebido.split("/");
                     try {
                         System.out.println("Name: " + vars[1]);
-                        if(vars[1].equalsIgnoreCase("all")){
+                        if (vars[1].equalsIgnoreCase("all")) {
                             System.out.println("Recebido " + vars[0] + " - " + pacote.getAddress());
+
+                            byte[] saida = new byte[1024];
+                            saida = ("[" + nick + "] " 
+                            + " /"
+                            + vars[0].replace("[", "").replace("]", "").trim() 
+                            + "/ Host").getBytes();
+
+                            pacote = new DatagramPacket(saida, saida.length, grupo, 5000);
+                            socket.send(pacote);
+
                         }
                         if (vars[1].equals(nick)) {
                             System.out.println("Received: " + recebido);
@@ -71,7 +88,7 @@ public class Main {
                         }
                     } catch (ArrayIndexOutOfBoundsException e) {
                     }
-                } catch (IOException e){
+                } catch (IOException e) {
                 }
             }
         }
